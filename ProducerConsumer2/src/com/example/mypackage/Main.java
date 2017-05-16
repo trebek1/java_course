89950660aeca8ad471d03cefaf53649197c9382c
Alex Trzeciak <trebek08@gmail.com>
2017-05-15 22:41:11 -0700

    Used lock interface instead of synchronized to create producer consumer example

--- ProducerConsumer2/src/com/example/mypackage/Main.java
+++ ProducerConsumer2/src/com/example/mypackage/Main.java
@@ -3,6 +3,7 @@ package com.example.mypackage;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 import static com.example.mypackage.Main.EOF;
 
@@ -12,9 +13,13 @@ public class Main {
 
     public static void main(String[] args) {
 	    List<String> buffer = new ArrayList<String>();
-	    MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW);
-	    MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE);
-	    MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN);
+	    ReentrantLock bufferLock = new ReentrantLock();
+	    // not all implementations of lock are reentrant
+        // all threads need to compete for same lock to prevent thread interference
+
+	        MyProducer producer = new MyProducer(buffer, ThreadColor.ANSI_YELLOW, bufferLock);
+	    MyConsumer consumer1 = new MyConsumer(buffer, ThreadColor.ANSI_PURPLE, bufferLock);
+	    MyConsumer consumer2 = new MyConsumer(buffer, ThreadColor.ANSI_CYAN, bufferLock);
 
 	    new Thread(producer).start();
 	    new Thread(consumer1).start();
@@ -26,10 +31,12 @@ class MyProducer implements Runnable {
 
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyProducer(List<String> buffer, String color) {
+    public MyProducer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     public void run(){
@@ -39,9 +46,16 @@ class MyProducer implements Runnable {
         for(String num : nums){
             try{
                 System.out.println( color + "adding..." + num);
-                synchronized(buffer){
+                bufferLock.lock();
+                try{
                     buffer.add(num);
+                } finally{
+                    // finally code is executed no matter what
+                    // if exception in try block still executed
+                    bufferLock.unlock();
                 }
+                // must manually unlock the lock after execution
+                //synchronized would automatially do this
 
                 Thread.sleep(random.nextInt(1000));
             }catch(InterruptedException e){
@@ -50,9 +64,13 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding EOF and exiting...");
-        synchronized(buffer){
-            buffer.add("EOF");
-        }
+            bufferLock.lock();
+            try{
+                buffer.add("EOF");
+            }finally{
+                bufferLock.unlock();
+            }
+
 
     }
 }
@@ -60,27 +78,38 @@ class MyProducer implements Runnable {
 class MyConsumer implements Runnable{
     private List<String> buffer;
     private String color;
+    private ReentrantLock bufferLock;
 
-    public MyConsumer(List<String> buffer, String color) {
+    public MyConsumer(List<String> buffer, String color, ReentrantLock bufferLock) {
         this.buffer = buffer;
         this.color = color;
+        this.bufferLock = bufferLock;
     }
 
     public void run(){
+
+        int counter = 0;
+
         while(true){
-
-            synchronized(buffer){
-                if(buffer.isEmpty()){
-                    continue;
+            // if the lock is available, do the code
+            if(bufferLock.tryLock()){
+                try{
+                    if(buffer.isEmpty()){
+                        continue;
+                    }
+                    System.out.println(color + "The counter = " + counter);
+                    if(buffer.get(0).equals(EOF)){
+                        System.out.println( color + "Exiting");
+                        break;
+                    }else{
+                        System.out.println( color + "removed " + buffer.remove(0));
+                    }
+                }finally{
+                    bufferLock.unlock();
                 }
-                if(buffer.get(0).equals(EOF)){
-                    System.out.println( color + "Exiting");
-                    break;
-                }else{
-                    System.out.println( color + "removed " + buffer.remove(0));
-                }
+            }else{
+                counter++;
             }
-
 
         }
     }
@@ -101,3 +130,15 @@ class MyConsumer implements Runnable{
 
 //-- Lock instance does not have these problems
 
+// cant release lock until lock count = 0
+
+// use a try finaly block to call unlock in one place
+//  code in critical section could throw exception not explicitly handled in try catch
+
+//java.lang.IllegalMonitorStateException if try to unlock a lock that the object doesnt own
+
+// fairness parameter can be added to use thread that has been waiting the longest
+
+// getQueuedLength method returns number of waiting threads
+
+// lock API http://docs.oracle.com/javase/8/docs/api/java/util/concurrent/locks/Lock.html
